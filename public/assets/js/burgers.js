2e8c0b664c11be806a73518f88ec23f48b1c592e
David Ready <davis_ready@hotmail.com>
2021-02-25 16:47:25 -0500

    first function

--- public/assets/js/burgers.js
+++ public/assets/js/burgers.js
@@ -3,3 +3,43 @@ document.addEventListener('DOMContentLoaded', (event) => {
   if (event) {
     console.info('DOM loaded');
   }
+
+  // UPDATE
+  const changeDevourBtns = document.querySelectorAll('.change-devour');
+
+  // Set up the event listener for the create button
+  if (changeDevourBtns) {
+    changeDevourBtns.forEach((button) => {
+      button.addEventListener('click', (e) => {
+        // Grabs the id of the element that goes by the name, "id"
+        const id = e.target.getAttribute('data-id');
+        const newDevour = e.target.getAttribute('data-newdevour');
+
+        const newDevourState = {
+            devoured: newDevour,
+        };
+
+        fetch(`/api/burgers/${id}`, {
+          method: 'PUT',
+          headers: {
+            Accept: 'application/json',
+            'Content-Type': 'application/json',
+          },
+
+          // make sure to serialize the JSON body
+          body: JSON.stringify(newDevourState),
+        }).then((response) => {
+          // Check that the response is all good
+          // Reload the page so the user can see the new quote
+          if (response.ok) {
+            console.log(`changed devour to: ${newDevour}`);
+            location.reload('/');
+          } else {
+            alert('something went wrong!');
+          }
+        });
+      });
+    });
+  }
+
+});
