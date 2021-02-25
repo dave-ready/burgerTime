7a7fe5b3956cdbc2bf06a8b5a8931e2f52cc8fda
David Ready <davis_ready@hotmail.com>
2021-02-25 12:22:22 -0500

    objToSql function

--- config/orm.js
+++ config/orm.js
@@ -10,3 +10,22 @@ const printQuestionMarks = (num) => {
 
   return arr.toString();
 };
+
+//function to convert object key/value pairs to SQL syntax
+const objToSql = (ob) => {
+    const arr = [];
+  
+    // Loop through the keys and push the key/value as a string int arr
+    for (const key in ob) {
+      let value = ob[key];
+      // Check to skip hidden properties
+      if (Object.hasOwnProperty.call(ob, key)) {
+        // If string with spaces - add quotations 
+        if (typeof value === 'string' && value.indexOf(' ') >= 0) {
+          value = `'${value}'`;
+        }
+        arr.push(`${key}=${value}`);
+      }
+    }
+    return arr.toString();
+  };
