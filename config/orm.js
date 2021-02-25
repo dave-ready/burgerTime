7fe27cc867ab2874288f2c98dd516fa9976396b5
David Ready <davis_ready@hotmail.com>
2021-02-25 12:05:00 -0500

    orm.js printQuestionMarks function

---
+++ config/orm.js
@@ -0,0 +1,12 @@
+const connection = require('./connection.js');
+
+//function for SQL syntax to add ?'s in query
+const printQuestionMarks = (num) => {
+  const arr = [];
+
+  for (let i = 0; i < num; i++) {
+    arr.push('?');
+  }
+
+  return arr.toString();
+};
