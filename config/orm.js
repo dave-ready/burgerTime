a9d658d8f899bb69aa2940f986e1ee35d962e545
David Ready <davis_ready@hotmail.com>
2021-02-25 12:30:01 -0500

    create and export orm object

--- config/orm.js
+++ config/orm.js
@@ -29,3 +29,72 @@ const objToSql = (ob) => {
     }
     return arr.toString();
   };
+
+  //Create orm object for all our SQL statement functions.
+
+const orm = {
+    all(tableInput, cb) {
+      const queryString = `SELECT * FROM ${tableInput};`;
+      connection.query(queryString, (err, result) => {
+        if (err) {
+          throw err;
+        }
+        cb(result);
+      });
+    },
+    create(table, cols, vals, cb) {
+      let queryString = `INSERT INTO ${table}`;
+  
+      queryString += ' (';
+      queryString += cols.toString();
+      queryString += ') ';
+      queryString += 'VALUES (';
+      queryString += printQuestionMarks(vals.length);
+      queryString += ') ';
+  
+      console.log(queryString);
+  
+      connection.query(queryString, vals, (err, result) => {
+        if (err) {
+          throw err;
+        }
+  
+        cb(result);
+      });
+    },
+    // An example of objColVals would be {burger_name: cheeseburger, devoured: true}
+    update(table, objColVals, condition, cb) {
+      let queryString = `UPDATE ${table}`;
+  
+      queryString += ' SET ';
+      queryString += objToSql(objColVals);
+      queryString += ' WHERE ';
+      queryString += condition;
+  
+      console.log(queryString);
+      connection.query(queryString, (err, result) => {
+        if (err) {
+          throw err;
+        }
+  
+        cb(result);
+      });
+    },
+    delete(table, condition, cb) {
+      let queryString = `DELETE FROM ${table}`;
+      queryString += ' WHERE ';
+      queryString += condition;
+  
+      connection.query(queryString, (err, result) => {
+        if (err) {
+          throw err;
+        }
+  
+        cb(result);
+      });
+    },
+  };
+  
+  // Export the orm object for the model (burger.js).
+  module.exports = orm;
+  
