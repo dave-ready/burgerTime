b63610c6877cdce02a190dd141dbbefa6eac029c
David Ready <davis_ready@hotmail.com>
2021-02-24 20:42:22 -0500

    seeds.sql

---
+++ db/seeds.sql
@@ -0,0 +1,12 @@
+INSERT INTO burgers (burger_name) 
+VALUES ('Molly');
+INSERT INTO burgers (burger_name) 
+VALUES ('Charlie');
+INSERT INTO burgers (burger_name, devoured) 
+VALUES ('Poppy', true);
+INSERT INTO burgers (burger_name, devoured) 
+VALUES ('Oscar', true);
+INSERT INTO burgers (burger_name, devoured) 
+VALUES ('Smudge', true);
+INSERT INTO burgers (burger_name) 
+VALUES ('Daisy');
