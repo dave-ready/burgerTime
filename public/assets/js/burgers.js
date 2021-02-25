5968463314a910a6f8e88342947390e7418f95d2
David Ready <davis_ready@hotmail.com>
2021-02-25 16:54:24 -0500

    Create burger button

--- public/assets/js/burgers.js
+++ public/assets/js/burgers.js
@@ -42,4 +42,39 @@ document.addEventListener('DOMContentLoaded', (event) => {
     });
   }
 
+  // CREATE
+  const createBurgerBtn = document.getElementById('create-form');
+
+  if (createBurgerBtn) {
+    createBurgerBtn.addEventListener('submit', (e) => {
+      e.preventDefault();
+
+      // Grabs the value of the textarea that goes by the name, "quote"
+      const newBurger = {
+        burger_name: document.getElementById('burger').value.trim(),
+        devoured: document.getElementById('devoured').checked,
+      };
+
+      // Send POST request to create a new quote
+      fetch('/api/burgers', {
+        method: 'POST',
+        headers: {
+          Accept: 'application/json',
+          'Content-Type': 'application/json',
+        },
+
+        // make sure to serialize the JSON body
+        body: JSON.stringify(newBurger),
+      }).then(() => {
+        // Empty the form
+        document.getElementById('burger').value = '';
+
+        // Reload the page so the user can see the new quote
+        console.log('Created a new burger!');
+        location.reload();
+      });
+    });
+  }
+
+
 });
