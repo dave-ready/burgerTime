58d1844065128f61daa6942a88727d4615ce2e27
David Ready <davis_ready@hotmail.com>
2021-03-07 12:44:51 -0500

    minor edits + comments

--- controllers/burgerController.js
+++ controllers/burgerController.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const burger = require('../models/burger.js');
-
 const router = express.Router();
 
 
@@ -17,7 +16,6 @@ router.get('/', (req, res) => {
 
 router.post('/api/burgers', (req, res) => {
     burger.create(['burger_name', 'devoured'], [req.body.burger_name, req.body.devoured], (result) => {
-      // Send back the ID of the new quote
       res.json({ id: result.insertId });
     });
   });
@@ -34,7 +32,6 @@ router.post('/api/burgers', (req, res) => {
       condition,
       (result) => {
         if (result.changedRows === 0) {
-          // If no rows were changed, then the ID must not exist, so 404
           return res.status(404).end();
         }
         res.status(200).end();
@@ -47,13 +44,12 @@ router.post('/api/burgers', (req, res) => {
   
     burger.delete(condition, (result) => {
       if (result.affectedRows === 0) {
-        // If no rows were changed, then the ID must not exist, so 404
         return res.status(404).end();
       }
       res.status(200).end();
     });
   });
   
-  // Export routes for server.js to use.
+  // Export routes for server.js 
   module.exports = router;
   
