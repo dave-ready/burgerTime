0104b65768fd16dab2f3d7a4b7778f3e08f30025
David Ready <davis_ready@hotmail.com>
2021-03-08 10:59:16 -0500

    JAWS edits

--- config/connection.js
+++ config/connection.js
@@ -5,16 +5,16 @@ const mysql = require('mysql');
 require("dotenv").config();
 
 if (process.env.JAWSDB_URL) {
-  connection = mysql.createConnection(process.env.JAWSDB_URL);
-   } else {
-    const connection = mysql.createConnection({
+    const connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    connection = mysql.createConnection({
       host: 'localhost',
       port: 3306,
       user: 'root',
       password: 'process.env.DB_Pass',
       database: 'burgers_db',
-   }
-});
+   });
+}
 
 
 connection.connect((err) => {
