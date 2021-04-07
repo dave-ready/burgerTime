7bb51101d5d44e1c5fdfef78b801bc7045a99389
David Ready <davis_ready@hotmail.com>
2021-04-07 18:56:54 -0400

    final deployment edits + file reconfig

--- controllers/burgerController.js
+++ controllers/burgerController.js
@@ -16,41 +16,40 @@ router.get('/', (req, res) => {
 });
 
 router.post('/api/burgers', (req, res) => {
-    burger.create(['burger_name', 'devoured'], [req.body.burger_name, false], (result) => {
-      res.json({ id: result.insertId });
-    });
+  burger.create(['burger_name', 'devoured'], [req.body.burger_name, req.body.devoured], (result) => {
+    res.json({ id: result.insertId });
   });
+});
 
-  router.put('/api/burgers/:id', (req, res) => {
-    const condition = `id = ${req.params.id}`;
-  
-    console.log('condition', condition);
-  
-    burger.update(
-      {
-        devoured: 1,
-      },
-      condition,
-      (result) => {
-        if (result.changedRows === 0) {
-          return res.status(404).end();
-        }
-        res.status(200).end();
-      }
-    );
-  });
+router.put('/api/burgers/:id', (req, res) => {
+  const condition = `id = ${req.params.id}`;
 
-  router.delete('/api/burgers/:id', (req, res) => {
-    const condition = `id = ${req.params.id}`;
-  
-    burger.delete(condition, (result) => {
-      if (result.affectedRows === 0) {
+  console.log('condition', condition);
+
+  burger.update(
+    {
+      devoured: req.body.devoured,
+    },
+    condition,
+    (result) => {
+      if (result.changedRows === 0) {
         return res.status(404).end();
       }
       res.status(200).end();
-    });
+    }
+  );
+});
+
+router.delete('/api/burgers/:id', (req, res) => {
+  const condition = `id = ${req.params.id}`;
+
+  burger.delete(condition, (result) => {
+    if (result.affectedRows === 0) {
+      return res.status(404).end();
+    }
+    res.status(200).end();
   });
-  
-  // Export routes for server.js 
-  module.exports = router;
-  
+});
+
+// Export routes for server.js 
+module.exports = router;
